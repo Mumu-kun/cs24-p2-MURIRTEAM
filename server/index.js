ce5ca13c1c4df3f62a61b62ae282d1d68980a0d6
sleepytmzd <depressionstonks420@gmail.com>
2024-03-30 21:24:24 +0600

    Update

--- server/index.js
+++ server/index.js
@@ -1,5 +1,6 @@
 import express from "express";
 import morgan from "morgan";
+import session from "express-session";
 import cors from "cors";
 import dbPromise from "./db_init.js";
 
@@ -10,29 +11,40 @@ app.use(morgan("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.get('/', async(req, res)=>{
+
+// Create a session for user
+const userSession = session({
+	name: 'userSession',
+	secret: 'user-secret-key',
+	resave: false,
+	saveUninitialized: false
+});
+
+app.use('/auth', userSession);
+
+app.get('/', async (req, res) => {
 	const db = await dbPromise;
 	try {
-	
+
 		const q1 = await db.all(`SELECT * FROM user`);
 		const q2 = await db.all(`SELECT * FROM role`);
 		const q3 = await db.all(`SELECT * FROM permission`);
 		const q4 = await db.all(`SELECT * FROM role_permission`);
 		const q5 = await db.all(`SELECT * FROM vehicle`);
 		const q6 = await db.all(`SELECT * FROM sts`);
-		res.send({q1, q2, q3, q4, q5, q6});
+		res.send({ q1, q2, q3, q4, q5, q6 });
 
 	} catch (error) {
 		console.error('error executing query: ', error);
-        res.status(500).json({ error: 'Internal Server Error' });
+		res.status(500).json({ error: 'Internal Server Error' });
 	}
 })
 
 // Get all permissions given to current user
-app.get('/permissions/:id', async(req, res)=>{
+app.get('/permissions/:id', async (req, res) => {
 	const db = await dbPromise;
 	try {
-		
+
 		const user_id = req.params.id;
 		const q = await db.all(`
 			SELECT P.*
@@ -46,45 +58,45 @@ app.get('/permissions/:id', async(req, res)=>{
 
 	} catch (error) {
 		console.error('error executing query: ', error);
-        res.status(500).json({ error: 'Internal Server Error' });
+		res.status(500).json({ error: 'Internal Server Error' });
 	}
 })
 
 // List all users
-app.get('/users', async(req, res) => {
-    const db = await dbPromise;
+app.get('/users', async (req, res) => {
+	const db = await dbPromise;
 	try {
-		
+
 		const q = await db.all(`SELECT * FROM user`);
 		res.send(q);
 
 	} catch (error) {
 		console.error('error executing query: ', error);
-        res.status(500).json({ error: 'Internal Server Error' });
+		res.status(500).json({ error: 'Internal Server Error' });
 	}
 });
 
 // Get a specific user details
-app.get('/users/:id', async(req, res) => {
-    const db = await dbPromise;
+app.get('/users/:id', async (req, res) => {
+	const db = await dbPromise;
 	try {
-		
+
 		const user_id = req.params.id;
 		const q = await db.all(`SELECT * FROM user WHERE id = ?`, user_id);
 		res.send(q);
 
 	} catch (error) {
 		console.error('error executing query: ', error);
-        res.status(500).json({ error: 'Internal Server Error' });
+		res.status(500).json({ error: 'Internal Server Error' });
 	}
 });
 
 // Create a new user
-app.post('/users', async(req, res) => {
-    const db = await dbPromise;
+app.post('/users', async (req, res) => {
+	const db = await dbPromise;
 	try {
-		
-		const {username, email, password} = req.body;
+
+		const { username, email, password } = req.body;
 		const q = await db.run(`
 			INSERT INTO user (username, email, password) VALUES (?, ?, ?)
 		`, [username, email, password]);
@@ -92,16 +104,16 @@ app.post('/users', async(req, res) => {
 
 	} catch (error) {
 		console.error('error executing query: ', error);
-        res.status(500).json({ error: 'Internal Server Error' });
+		res.status(500).json({ error: 'Internal Server Error' });
 	}
 });
 
 // Assign a role to a user
-app.post('/users', async(req, res) => {
-    const db = await dbPromise;
+app.post('/users/:userID/roles', async (req, res) => {
+	const db = await dbPromise;
 	try {
-		
-		const {user_id, role_id} = req.body;
+
+		const { user_id, role_id } = req.body;
 		const q = await db.run(`
 			UPDATE user SET role_id = ? WHERE id = ?
 		`, [role_id, user_id]);
@@ -109,16 +121,63 @@ app.post('/users', async(req, res) => {
 
 	} catch (error) {
 		console.error('error executing query: ', error);
-        res.status(500).json({ error: 'Internal Server Error' });
+		res.status(500).json({ error: 'Internal Server Error' });
 	}
 });
 
-// Create STS
-app.post('/create/STS', async(req, res)=>{
+app.delete('/users/:id', async (req, res) => {
+
 	const db = await dbPromise;
 	try {
-		
-		const {ward_number, capacity, latitude, longitude} = req.body;
+
+		const user_id = req.params.id;
+		const q = await db.run(`DELETE FROM user WHERE id = ?`, user_id);
+		res.send(q);
+
+	} catch (error) {
+		console.error('error executing query: ', error);
+		res.status(500).json({ error: 'Internal Server Error' });
+	}
+
+});
+
+app.get('users/roles', async (req, res) => {
+	const db = await dbPromise;
+	try {
+
+		const q = await db.all(`SELECT * FROM role`);
+		res.send(q);
+	} catch (error) {
+		console.error('error executing query: ', error);
+		res.status(500).json({ error: 'Internal Server Error' });
+	}
+})
+
+app.post('rbac/roles', async (req, res) => {
+	const db = await dbPromise;
+	try {
+
+		const { id, name } = req.body;
+		const q = await db.all(`SELECT * FROM role where id = ?`, [id]);
+		if (q.length > 0) {
+			const r = await db.run(`UPDATE role SET name = ? WHERE id = ?`, [name, id]);
+		}
+		else {
+			const r = await db.run(`INSERT INTO role (name) VALUES (?)`, [name]);
+		}
+		res.send(r);
+	} catch (error) {
+		console.error('error executing query: ', error);
+		res.status(500).json({ error: 'Internal Server Error' });
+	}
+})
+
+// Create STS
+app.post('/create/STS', async (req, res) => {
+	const db = await dbPromise;
+	try {
+
+		const { ward_number, capacity, latitude, longitude } = req.body;
 		const q = await db.run(`
 			INSERT INTO sts (ward_number, capacity, latitude, longitude)
 			VALUES (?, ?, ?, ?)
@@ -127,16 +186,16 @@ app.post('/create/STS', async(req, res)=>{
 
 	} catch (error) {
 		console.error('error executing query: ', error);
-        res.status(500).json({ error: 'Internal Server Error' });
+		res.status(500).json({ error: 'Internal Server Error' });
 	}
 })
 
 // Assign STS manager
-app.post('/assign/STS_manager', async(req, res)=>{
+app.post('/assign/STS_manager', async (req, res) => {
 	const db = await dbPromise;
 	try {
-		
-		const {user_id, sts_id} = req.body;
+
+		const { user_id, sts_id } = req.body;
 		const q = await db.run(`
 			INSERT INTO sts_manager (user_id, sts_id)
 			VALUES (?, ?)
@@ -145,16 +204,16 @@ app.post('/assign/STS_manager', async(req, res)=>{
 
 	} catch (error) {
 		console.error('error executing query: ', error);
-        res.status(500).json({ error: 'Internal Server Error' });
+		res.status(500).json({ error: 'Internal Server Error' });
 	}
 })
 
 // Create vehicle
-app.post('/create/vehicle', async(req, res)=>{
+app.post('/create/vehicle', async (req, res) => {
 	const db = await dbPromise;
 	try {
-		
-		const {reg_num, type, capacity, fuel_cost_loaded, fuel_cost_unloaded} = req.body;
+
+		const { reg_num, type, capacity, fuel_cost_loaded, fuel_cost_unloaded } = req.body;
 		const q = await db.run(`
 			INSERT INTO vehicle (reg_num, type, capacity, fuel_cost_loaded, fuel_cost_unloaded)
 			VALUES (?, ?, ?, ?, ?)
@@ -163,30 +222,30 @@ app.post('/create/vehicle', async(req, res)=>{
 
 	} catch (error) {
 		console.error('error executing query: ', error);
-        res.status(500).json({ error: 'Internal Server Error' });
+		res.status(500).json({ error: 'Internal Server Error' });
 	}
 })
 
 // Get list of unassigned vehicles
-app.get('/vehicles/unassigned', async(req, res)=>{
+app.get('/vehicles/unassigned', async (req, res) => {
 	const db = await dbPromise;
 	try {
-		
+
 		const q = await db.all(`SELECT * FROM vehicle WHERE sts_id IS NULL`);
 		res.send(q);
 
 	} catch (error) {
 		console.error('error executing query: ', error);
-        res.status(500).json({ error: 'Internal Server Error' });
+		res.status(500).json({ error: 'Internal Server Error' });
 	}
 })
 
 // Assign vehicle to sts
-app.post('/assign/vehicle', async(req, res)=>{
+app.post('/assign/vehicle', async (req, res) => {
 	const db = await dbPromise;
 	try {
-		
-		const {reg_num, sts_id} = req.body;
+
+		const { reg_num, sts_id } = req.body;
 		const q = await db.run(`
 			UPDATE vehicle SET sts_id = ? WHERE reg_num = ?
 		`, [sts_id, reg_num]);
@@ -194,16 +253,16 @@ app.post('/assign/vehicle', async(req, res)=>{
 
 	} catch (error) {
 		console.error('error executing query: ', error);
-        res.status(500).json({ error: 'Internal Server Error' });
+		res.status(500).json({ error: 'Internal Server Error' });
 	}
 })
 
 // Add entry of vehicles leaving sts
-app.post('/entry/STS', async(req, res)=>{
+app.post('/entry/STS', async (req, res) => {
 	const db = await dbPromise;
 	try {
-		
-		const {sts_id, landfill_id, vehicle_num, weight, sts_arrival_time, sts_departure_time} = req.body;
+
+		const { sts_id, landfill_id, vehicle_num, weight, sts_arrival_time, sts_departure_time } = req.body;
 		const q = await db.run(`
 			INSERT INTO transport_record (sts_id, landfill_id, vehicle_num, weight, sts_arrival_time, sts_departure_time)
 			VALUES (?, ?, ?, ?, ?, ?)
@@ -212,16 +271,16 @@ app.post('/entry/STS', async(req, res)=>{
 
 	} catch (error) {
 		console.error('error executing query: ', error);
-        res.status(500).json({ error: 'Internal Server Error' });
+		res.status(500).json({ error: 'Internal Server Error' });
 	}
 })
 
 // Create Landfill
-app.post('/create/landfil', async(req, res)=>{
+app.post('/create/landfil', async (req, res) => {
 	const db = await dbPromise;
 	try {
-		
-		const {capacity, latitude, longitude, operational_span} = req.body;
+
+		const { capacity, latitude, longitude, operational_span } = req.body;
 		const q = await db.run(`
 			INSERT INTO landfill (capacity, latitude, longitude, operational_span)
 			VALUES (?, ?, ?, ?)
@@ -230,16 +289,16 @@ app.post('/create/landfil', async(req, res)=>{
 
 	} catch (error) {
 		console.error('error executing query: ', error);
-        res.status(500).json({ error: 'Internal Server Error' });
+		res.status(500).json({ error: 'Internal Server Error' });
 	}
 })
 
 // Assign landfill manager
-app.post('/assign/landfill_manager', async(req, res)=>{
+app.post('/assign/landfill_manager', async (req, res) => {
 	const db = await dbPromise;
 	try {
-		
-		const {user_id, landfill_id} = req.body;
+
+		const { user_id, landfill_id } = req.body;
 		const q = await db.run(`
 			INSERT INTO landfill (user_id, landfill_id)
 			VALUES (?, ?)
@@ -248,16 +307,16 @@ app.post('/assign/landfill_manager', async(req, res)=>{
 
 	} catch (error) {
 		console.error('error executing query: ', error);
-        res.status(500).json({ error: 'Internal Server Error' });
+		res.status(500).json({ error: 'Internal Server Error' });
 	}
 })
 
 // Add entry of truck dumping
-app.post('/entry/landfill', async(req, res)=>{
+app.post('/entry/landfill', async (req, res) => {
 	const db = await dbPromise;
 	try {
-		
-		const {sts_id, landfill_id, vehicle_num, landfill_arrival_time, landfill_departure_time} = req.body;
+
+		const { sts_id, landfill_id, vehicle_num, landfill_arrival_time, landfill_departure_time } = req.body;
 		const q = await db.run(`
 			UPDATE transport_record SET landfill_arrival_time = ? , landfill_departure_time = ?
 			WHERE sts_id = ? AND landfill_id = ? AND vehicle_num = ?
@@ -266,8 +325,87 @@ app.post('/entry/landfill', async(req, res)=>{
 
 	} catch (error) {
 		console.error('error executing query: ', error);
-        res.status(500).json({ error: 'Internal Server Error' });
+		res.status(500).json({ error: 'Internal Server Error' });
 	}
+})
+
+//AUTH ROUTES
+app.post('/auth/login', async (req, res) => {
+	const db = await dbPromise;
+	try {
+
+		const { email, password } = req.body;
+		const q = await db.all(`
+			SELECT * FROM user WHERE email = ? AND password = ?
+		`, [email, password]);
+		if (q.length > 0 && q[0].role_id == 1) {
+			res.send({ message: "No login available for unassigned role" });
+		}
+		else {
+			console.log(q);
+			req.session.user = q[0]; //storing user details in session
+			console.log(req.session.user);
+			res.send(q);
+		}
+
+
+	} catch (error) {
+		console.error('error executing query: ', error);
+		res.status(500).json({ error: 'Internal Server Error' });
+	}
+})
+
+app.get('/auth/logout', async (req, res) => {
+	req.session.destroy();
+	res.send({ message: "Logged out" });
+})
+
+//CHANGE PASSWORD IMPLEMENTATION REMAINING
+//END OF AUTH ROUTES
+
+//ROLE + PERMISSION ROUTES
+app.get('rbac/roles', async (req, res) => {
+	const db = await dbPromise;
+	try {
+
+		const q = await db.all(`SELECT * FROM role`);
+		res.send(q);
+	} catch (error) {
+		console.error('error executing query: ', error);
+		res.status(500).json({ error: 'Internal Server Error' });
+	}
+})
+
+
+app.get('rbac/permissions', async (req, res) => {
+	const db = await dbPromise;
+	try {
+
+		const q = await db.all
+			(`SELECT RP.role_id, RP.permission_id, P.description FROM role_permission RP JOIN permission P ON RP.permission_id = P.id`);
+		res.send(q);
+	} catch (error) {
+		console.error('error executing query: ', error);
+		res.status(500).json({ error: 'Internal Server Error' });
+	}
+})
+
+
+
+// FIX INSERTING INTO ROLE_PERMISSION TABLE
+app.post('rbac/permissions', async (req, res) => {
+	const db = await dbPromise;
+	try {
+
+		const { role_id, permission_id, description } = req.body;
+
+		let q = await db.run(`INSERT INTO permission (description) VALUES (?)`, [description]);
+
+	} catch (error) {
+		console.error('error executing query: ', error);
+		res.status(500).json({ error: 'Internal Server Error' });
+	}
+
 })
 
 app.listen(8000, () => {
